fface497f97742bf751f956d5822d464853e6555
Sunkai233 <jinhuiyang964@gmail.com>
2025-09-23 19:40:54 +0800

    Add files via upload

---
+++ 复现6.25wm风机步骤/driver.py
@@ -0,0 +1,21 @@
+import os
+from weis import weis_main
+
+# 如果要做快速测试，可以设 True 缩短仿真时间
+TEST_RUN = False
+
+# 当前 driver.py 所在目录
+run_dir = os.path.dirname(os.path.realpath(__file__))
+
+# 你的三个输入文件路径
+fname_wt_input = os.path.join(run_dir, "nrel5mw.yaml")
+fname_modeling_options = os.path.join(run_dir, "modeling_options.yaml")
+fname_analysis_options = os.path.join(run_dir, "analysis_options.yaml")
+
+# 调用 WEIS 主入口
+wt_opt, modeling_options, opt_options = weis_main(
+    fname_wt_input,
+    fname_modeling_options,
+    fname_analysis_options,
+    test_run=TEST_RUN
+)
